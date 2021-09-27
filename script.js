4fef03c25d2619f1ae38069999ff5bc85564461e
Chiaration <45766347+Chiaration@users.noreply.github.com>
2021-09-27 18:48:49 +0100

    Added calculator functions

---
+++ script.js
@@ -0,0 +1,23 @@
+//Adding Function
+function add(a,b) {
+    return a+b;
+}
+
+//Substracting Function
+function subtract(a,b) {
+    return a - b;
+}
+
+//Multiplying Function
+function multiply(a,b) {
+    return a*b;
+}
+
+//Dividing Function
+function divide(a, b) {
+    if (b === 0) {
+        return "ERROR"
+    } else {
+        return a/b;
+    }
+}
