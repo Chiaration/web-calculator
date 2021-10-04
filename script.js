47411f018afebf9ae3da66cdcf633e09444353ab
Chiaration <45766347+Chiaration@users.noreply.github.com>
2021-10-04 16:54:15 +0100

    Functional operator buttons Functional display of numbers

--- script.js
+++ script.js
@@ -28,16 +28,48 @@ function operate(num1, num2, operator) {
         return add(num1, num2);
     } else if (operator == "-") {
         return subtract(num1, num2);
-    } else if (operator == "/") {
+    } else if (operator == "÷") {
         return divide(num1, num2);
-    } else if (operator == "*") {
+    } else if (operator == "×") {
         return multiply(num1, num2);
     }
 }
 
-const number_buttons = document.querySelectorAll('button.number')
+let x = '', y = '';
+let is_x = false;
+
+const number_buttons = document.querySelectorAll('button.number');
 number_buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        alert(button.textContent)
+        const calculator_main_screen = document.querySelector('#calculator-display-main')
+        if (is_x == false) {
+            x += String(button.id)
+            calculator_main_screen.innerHTML = x;
+        } else {
+            y += String(button.id)
+            calculator_main_screen.innerHTML = y;
+        }
     })
 })
+
+const operator_buttons = document.querySelectorAll('button.operator');
+operator_buttons.forEach((button) => {
+    const calculator_main_screen = document.querySelector('#calculator-display-main')
+    const calculator_mini_screen = document.querySelector('#calculator-display-mini')
+    button.addEventListener('click', () => {
+        if (button.id != '=') {
+            if (is_x == false) {
+                calculator_mini_screen.innerHTML = calculator_main_screen.innerHTML + ` ${button.id} `;
+                calculator_main_screen.innerHTML = '&nbsp;';
+                is_x = true;
+            } else {
+                calculator_mini_screen.innerHTML += y + ` ${button.id} `;
+                x = operate(parseFloat(x), parseFloat(y), button.id);
+                console.log(x);
+                y = ''
+                calculator_main_screen.innerHTML = x;
+            }
+        }
+    })
+})
+
